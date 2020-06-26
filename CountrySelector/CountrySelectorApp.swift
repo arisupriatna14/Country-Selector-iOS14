6b6a77c871b7e319874baf1c06d67a7959ee248e
arisupriatna14 <arisupriatna703@gmail.com>
2020-06-26 23:37:17 +0700

    Initial Commit

---
+++ CountrySelector/CountrySelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountrySelectorApp.swift
+//  CountrySelector
+//
+//  Created by Ari Supriatna on 26/06/20.
+//
+
+import SwiftUI
+
+@main
+struct CountrySelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
